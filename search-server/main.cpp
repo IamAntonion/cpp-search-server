f459e216b93ef5bb64b2099acde106c8fb81d2ef
Anton Chemodanov <117853170+IamAntonion@users.noreply.github.com>
2023-07-03 14:16:46 +0300

    Update main.cpp

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -8,6 +8,7 @@
 #include <vector>
 #include <optional>
 #include <numeric>
+#include <deque>
  
 using namespace std;
  
@@ -372,6 +373,46 @@ public:
 private:
     vector<IteratorRange<Iterator>> pages_;
 };
+
+class RequestQueue {
+public:
+    explicit RequestQueue(const SearchServer& search_server) : search_request(search_server){}
+ 
+    template <typename DocumentPredicate>
+    vector<Document> AddFindRequest(const string& raw_query, DocumentPredicate document_predicate) {
+ 
+        vector<Document> helper = search_request.FindTopDocuments(raw_query, document_predicate);
+        
+        QueryResult query;
+        query.query_result = (helper.empty()==false);
+        
+        if (!(requests_.size() < min_in_day_)){
+            requests_.pop_front();
+        } 
+        requests_.push_back(query); 
+        return helper;
+    }
+    
+    vector<Document> AddFindRequest(const string& raw_query, DocumentStatus status) {
+         return AddFindRequest(raw_query, [&status](int document_id, DocumentStatus document_status, int rating) {
+            return document_status == status;});
+    }
+    
+    vector<Document> AddFindRequest(const string& raw_query) {
+        return AddFindRequest(raw_query, DocumentStatus::ACTUAL);
+    }
+    
+    int GetNoResultRequests() const {
+        return count_if(requests_.begin(), requests_.end(), [](QueryResult query) {return query.query_result == 0;});
+    }
+private:
+    struct QueryResult {
+        bool query_result;
+    };
+    const SearchServer& search_request;
+    deque<QueryResult> requests_;
+    const static int min_in_day_ = 1440;
+}; 
 
 // ==================== для примера =========================
 template <typename Container>
