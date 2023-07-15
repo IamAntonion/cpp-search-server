3268e609c5f55900966bd7d42c185fe992e8a0c7
Anton Chemodanov <117853170+IamAntonion@users.noreply.github.com>
2023-07-15 13:24:38 +0300

    Add files via upload

--- search-server/search_server.h
+++ search-server/search_server.h
@@ -9,8 +9,9 @@
 #include <string>
 #include <vector>
 #include "read_input_functions.h"
-#include "string_processing.h" 
+#include "string_processing.h"
 #include "document.h"
+#include "log_duration.h" 
  
 using namespace std::string_literals;
  
@@ -19,15 +20,23 @@ public:
     template <typename StringContainer>
     SearchServer(const StringContainer& stop_words);
     SearchServer(const std::string& stop_words_text) : SearchServer(SplitIntoWords(stop_words_text)){}
+    
     void AddDocument(int document_id, const std::string& document, DocumentStatus status, const std::vector<int>& ratings);
     
     template <typename DocumentPredicate>
     std::vector<Document> FindTopDocuments(const std::string& raw_query, DocumentPredicate document_predicate) const;
-    
     std::vector<Document> FindTopDocuments(const std::string& raw_query, DocumentStatus status) const;
     std::vector<Document> FindTopDocuments(const std::string& raw_query) const;
+    
     int GetDocumentCount() const;
-    int GetDocumentId(int index) const;
+    
+    std::set<int> ::const_iterator begin() const;
+    std::set<int> ::const_iterator end() const;
+    
+    const std::map<std::string, double>& GetWordFrequencies(int document_id) const;
+    
+    void RemoveDocument(int document_id);
+    
     std::tuple<std::vector<std::string>, DocumentStatus> MatchDocument(const std::string& raw_query, int document_id) const;
  
 private:
@@ -42,7 +51,8 @@ private:
     const std::set<std::string> stop_words_;
     std::map<std::string, std::map<int, double>> word_to_document_freqs_;
     std::map<int, DocumentData> documents_;
-    std::vector<int> document_ids_;
+    std::set<int> document_ids_;
+    std::map<int, std::map<std::string, double>> ids_of_docs_to_word_freqs_;
  
     bool IsStopWord(const std::string& word) const;
     static bool IsValidWord(const std::string& word);
